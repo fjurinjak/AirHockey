f7ed0dad53f24295da2890049a281ac3ba8e4bd9
Filip Jurinjak <filipjurinjak1@gmail.com>
2020-07-11 18:11:24 +0200

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject DifficultyToggles;
+
+    public void Start()
+    {
+        DifficultyToggles.transform.GetChild((int)GameValues.Difficulty).GetComponent<Toggle>().isOn = true;
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+    #region Difficulty
+    public void SetEasyDifficulty(bool isOn)
+    {
+        if (isOn)
+            GameValues.Difficulty = GameValues.Difficulties.Easy;
+    }
+
+    public void SetMediumDifficulty(bool isOn)
+    {
+        if (isOn)
+            GameValues.Difficulty = GameValues.Difficulties.Medium;
+    }
+
+    public void SetHardDifficulty(bool isOn)
+    {
+        if (isOn)
+            GameValues.Difficulty = GameValues.Difficulties.Hard;
+    }
+    #endregion
+}
